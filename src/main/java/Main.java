d6a803d1ae09afc04d5a21d3321d1685a46fed51
Endermaiter <mpereznovoa@danielcastelao.org>
2022-03-04 09:35:56 +0100

    Segunda rama fichero

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,12 +7,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        GitHub github = null;
         try {
-            GitHub github = null;
-            GitHub gitub = new GitHubBuilder().withOAuthToken("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").build();
-            //GHRepository repo2 = github.createRepository("nuevo_repositorio_con_java").create();
-            GHRepository rep = github.createRepository("nuevo_repositorio_con_java","Este es mi primer repositorio creado con java", "https://github.com/Endermaiter",true/*public*/);
+
+            String pathAlFich = '';
+            github = new GitHubBuilder().fromPropertyFile(pathAlFich).build();
         } catch (IOException e) {
             e.printStackTrace();
         }
