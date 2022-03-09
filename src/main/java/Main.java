309a4fe0a3312e15a33882ced8037d2c289c7d1e
Endermaiter <mpereznovoa@danielcastelao.org>
2022-03-09 13:34:41 +0100

    ACCESS CON TOKEN EN ARCHIVO FUNCIONA

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,8 +10,16 @@ public class Main {
         GitHub github = null;
         try {
 
-            String pathAlFich = '';
-            github = new GitHubBuilder().fromPropertyFile(pathAlFich).build();
+            String pathAlFich = "/home/dam1/Documentos/ENDERMAITER/COD/accessToken.properties";
+            github = new GitHubBuilder()
+                    .fromPropertyFile(pathAlFich)
+                    .build();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            GHRepository repo = github.createRepository("new-repository").create();
         } catch (IOException e) {
             e.printStackTrace();
         }
