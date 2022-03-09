185227f32d4b2a6d2b4b59ca8169da42618331e7
Endermaiter <mpereznovoa@danielcastelao.org>
2022-03-09 13:46:52 +0100

    ACCESS CON TOKEN EN EL CODIGO FUNCIONA

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import org.kohsuke.github.*;
 
+import javax.swing.*;
 import java.io.IOException;
 
 
@@ -7,16 +8,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        //en la rama master tenemos la creacion del repositoro con el token insertado en el codigo.
+        GitHub github = null;
         try {
-            GitHub github = null;
-            GitHub gitub = new GitHubBuilder().withOAuthToken("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").build();
-            //GHRepository repo2 = github.createRepository("nuevo_repositorio_con_java").create();
-            GHRepository rep = github.createRepository("nuevo_repositorio_con_java","Este es mi primer repositorio creado con java", "https://github.com/Endermaiter",true/*public*/);
+            github = new GitHubBuilder().withOAuthToken("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").build();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        try {
+            GHRepository repo = github.createRepository("new-repository").create();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
